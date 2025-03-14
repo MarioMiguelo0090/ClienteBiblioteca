fa1085879d8eb7b8e423fbae673c9309eed05cf7
JosueMG15 <159586895+JosueMG15@users.noreply.github.com>
2025-03-14 13:12:20 -0600

    Estructura inicial del cliente

---
+++ Ventanas/VentanaMenuPrincipalBibliotecario.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ClienteBibliotecaElSaber.Ventanas
+{
+    /// <summary>
+    /// Lógica de interacción para VentanaMenuPrincipal.xaml
+    /// </summary>
+    public partial class VentanaMenuPrincipalBibliotecario : Window
+    {
+        public VentanaMenuPrincipalBibliotecario()
+        {
+            InitializeComponent();
+        }
+
+        private void Grid_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                DragMove();
+            }
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
