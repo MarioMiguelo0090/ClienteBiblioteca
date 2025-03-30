29ef8e904766f2c767d2f14acb6dd63b32f3942d
Hizay Apodaca Garcia <hizzay@hotmail.com>
2025-03-30 13:31:53 -0600

    Ventanas Archivar, editar libro, editar socio

---
+++ Ventanas/VentanaEditarLibro.xaml.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ClienteBibliotecaElSaber.Ventanas
+{
+    /// <summary>
+    /// Lógica de interacción para VentanaEditarLibro.xaml
+    /// </summary>
+    public partial class VentanaEditarLibro : Window
+    {
+        public VentanaEditarLibro()
+        {
+            InitializeComponent();
+        }
+
+        private void SubirImagen_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void txb_imagen_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void Aceptar_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Cancelar_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void txb_Titulo_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void txb_Autor_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void txb_Genero_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void txb_Estado_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void txb_CantidadEjemplares_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void txb_FechaPublicacion_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void txb_Editorial_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void txb_NumeroDePaginas_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Ventanas/VentanaEditarSocio.xaml.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ClienteBibliotecaElSaber.Ventanas
+{
+    /// <summary>
+    /// Lógica de interacción para VentanaEditarSocio.xaml
+    /// </summary>
+    public partial class VentanaEditarSocio : Window
+    {
+        public VentanaEditarSocio()
+        {
+            InitializeComponent();
+        }
+
+        private void txb_nombre_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void txb_primerApellido_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void txb_segundoApellido_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void txb_telefono_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void txb_direccion_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void txb_codigoPostal_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void txb_fechaDeNacimiento_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void txb_fechaDeInscripcion_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void Cancelar_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Aceptar_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
