7f9949eb0b9aaed7a1126ff68a6a1765ed34e5da
JosueMG15 <159586895+JosueMG15@users.noreply.github.com>
2025-04-02 10:01:50 -0600

    Ventanas Buscar Préstamo, Consultar préstamos de socio, Editar préstamo y Gestionar multa

---
+++ Ventanas/VentanaBuscarPrestamo.xaml.cs
@@ -0,0 +1,38 @@
+﻿using ClienteBibliotecaElSaber.ServidorElSaber;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ClienteBibliotecaElSaber.Ventanas
+{
+    public partial class VentanaBuscarPrestamo : Window
+    {
+        public ObservableCollection<PrestamoBinding> Prestamos { get; set; }
+        public VentanaBuscarPrestamo()
+        {
+            InitializeComponent();
+            DataContext = this;
+        }
+
+        private void Cancelar_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Editar_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Ventanas/VentanaConsultarPrestamosSocio.xaml.cs
@@ -0,0 +1,28 @@
+﻿using ClienteBibliotecaElSaber.ServidorElSaber;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ClienteBibliotecaElSaber.Ventanas
+{
+    public partial class VentanaConsultarPrestamosSocio : Window
+    {
+        public ObservableCollection<PrestamoBinding> Prestamos { get; set; }
+        public VentanaConsultarPrestamosSocio()
+        {
+            InitializeComponent();
+            DataContext = this;
+        }
+    }
+}
