6954565da6c3b6f66d6843936461642b0bd8c8b8
Hizay Apodaca Garcia <hizzay@hotmail.com>
2025-04-03 10:49:21 -0600

    Ventana Editar Libro

--- Ventanas/VentanaEditarLibro.xaml.cs
+++ Ventanas/VentanaEditarLibro.xaml.cs
@@ -1,35 +1,29 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using ClienteBibliotecaElSaber.ServidorElSaber;
+using Microsoft.Win32;
+using System;
+using System.IO;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 
 namespace ClienteBibliotecaElSaber.Ventanas
 {
-    /// <summary>
-    /// Lógica de interacción para VentanaEditarLibro.xaml
-    /// </summary>
     public partial class VentanaEditarLibro : Window
     {
-        public VentanaEditarLibro()
+        private LibroBinding libro;
+        private byte[] nuevaImagen;
+
+        public VentanaEditarLibro(LibroBinding datosDelLibro)
         {
             InitializeComponent();
+            CargarDatosDeLibro(datosDelLibro);
+            this.libro = datosDelLibro;
+        }
+
+        private void CargarDatosDeLibro(LibroBinding datosLibro)
+        {
+
         }
 
         private void SubirImagen_Click(object sender, RoutedEventArgs e)
-        {
-
-        }
-
-        private void txb_imagen_TextChanged(object sender, TextChangedEventArgs e)
         {
 
         }
@@ -41,47 +35,7 @@ namespace ClienteBibliotecaElSaber.Ventanas
 
         private void Cancelar_Click(object sender, RoutedEventArgs e)
         {
-
-        }
-
-        private void txb_Titulo_TextChanged(object sender, TextChangedEventArgs e)
-        {
-
-        }
-
-        private void txb_Autor_TextChanged(object sender, TextChangedEventArgs e)
-        {
-
-        }
-
-        private void txb_Genero_TextChanged(object sender, TextChangedEventArgs e)
-        {
-
-        }
-
-        private void txb_Estado_TextChanged(object sender, TextChangedEventArgs e)
-        {
-
-        }
-
-        private void txb_CantidadEjemplares_TextChanged(object sender, TextChangedEventArgs e)
-        {
-
-        }
-
-        private void txb_FechaPublicacion_TextChanged(object sender, TextChangedEventArgs e)
-        {
-
-        }
-
-        private void txb_Editorial_TextChanged(object sender, TextChangedEventArgs e)
-        {
-
-        }
-
-        private void txb_NumeroDePaginas_TextChanged(object sender, TextChangedEventArgs e)
-        {
-
+            this.Close();
         }
     }
 }
